e1e48181ef4ff0e521b2c1a6008d2dd03f105582
Sharath George M <sharathgeorgem@gmail.com>
2019-03-26 15:31:42 +0530

    Added dgram, buffer and url libraries

--- index.js
+++ index.js
@@ -3,6 +3,26 @@
 const fs = require('fs')
 const bencode = require('bencode')
 
-const torrent = bencode.decode(fs.readFileSync('prog_rock.torrent'))
+const dgram = require('dgram')
+const { Buffer } = require('buffer')
+const urlParse = require('url').parse
 
-console.log(torrent.announce.toString('utf8'))
+const torrent = bencode.decode(fs.readFileSync('beast.torrent'))
+
+// Announce property on the buffer used to get the URL for the tracker.
+const url = urlParse(torrent.announce.toString('utf-8'))
+
+const socket = dgram.createSocket('udp4')
+
+const myMsg = Buffer.from('hello?', 'utf-8')
+
+socket.send(myMsg, 0, myMsg.length, url.port, url.host, () => {
+  console.log('MESSAGE TRANSMITTED')
+})
+
+socket.on('message', msg => {
+  console.log('The UDP msg received is ', msg)
+})
+
+console.log('The message is given as ', myMsg.toString('utf-8'))
+console.log('Protocol`', url.protocol)
