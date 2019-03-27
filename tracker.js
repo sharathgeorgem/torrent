2e5e2b658fd3cdf53de38379a05f74b127e3100e
Sharath George M <sharathgeorgem@gmail.com>
2019-03-27 12:50:50 +0530

    Implemented Connect request builder and Connect response parser

--- tracker.js
+++ tracker.js
@@ -2,17 +2,18 @@
 
 const dgram = require('dgram')
 const urlParse = require('url').parse()
+const crypto = require('crypto') // Buffer encryption
 
 module.exports.getPeers = (torrent, callback) => {
   const socket = dgram.createSocket('udp4')
   const url = torrent.announce.toString('utf-8')
 
-  udpSend(socket, buildConnectionRequest(), url)
+  udpSend(socket, buildConnectRequest(), url)
 
   socket.on('message', response => {
     if (responseType(response) === 'connect') {
-      const connectionResponse = parseConnectionResponse(response)
-      const announceRequest = buildAnnounceRequest(connectionResponse.connectionId)
+      const connectResponse = parseConnectResponse(response)
+      const announceRequest = buildAnnounceRequest(connectResponse.connectionId)
       console.log(response)
       udpSend(socket, announceRequest, url)
 
@@ -29,11 +30,40 @@ function udpSend (socket, message, rawURL, callback = () => {}) {
   socket.send(message, 0, message.length, url.port, url.host, callback)
 }
 
+function buildConnectRequest () {
+  /* Connect Request format
+    Offset  Size            Name            Value
+    0       64-bit integer  connection_id   0x41727101980
+    8       32-bit integer  action          0 // connect
+    12      32-bit integer  transaction_id  ? // random
+    16
+  */
+  const buffer = Buffer.alloc(16)
+  // connection_id
+  buffer.writeUInt32BE(0x417, 0)
+  buffer.writeUInt32BE(0x27101980, 4)
+  // action
+  buffer.writeUInt32BE(0, 8)
+  // transaction_id
+  crypto.randomBytes(4).copy(buffer, 12)
+}
+
 function responseType (response) {}
 
-function buildConnectionRequest () {}
-
-function parseConnectionResponse () {}
+function parseConnectResponse(response) {
+  /* Connect Response format
+  Offset  Size            Name            Value
+  0       32-bit integer  action          0 // connect
+  4       32-bit integer  transaction_id
+  8       64-bit integer  connection_id
+  16
+  */
+  return {
+    action: response.readUInt32BE(0),
+    transactionId: response.readUInt32BE(4),
+    connectionId: response.slice(8) // Unable to read as 64 bit Integer.
+  }
+}
 
 function buildAnnounceRequest () {}
 
